cecd451a23deafa7f826d0cb4167c4c0f9fc9876
Gonzalo Parra <gonzzaparra@gmail.com>
2024-04-24 09:19:12 -0300

    feat(components): add section component to handle sections in the app

---
+++ src/components/ui/section.tsx
@@ -0,0 +1,20 @@
+import type React from 'react';
+import { cn } from '@/lib/utils';
+
+export type BadgeProps = React.HTMLAttributes<HTMLDivElement>;
+
+export function Section({
+  className,
+  ...props
+}: BadgeProps) {
+  return (
+    <section
+      id={props.id}
+      className={cn(
+        'flex flex-col min-h-0 gap-y-4 scroll-mt-24 w-full md:max-w-3xl mx-auto',
+        className
+      )}
+      {...props}
+    />
+  )
+}
