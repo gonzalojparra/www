5cd944f4cdbb83d0de0d73108ed2590975024699
Gonzalo Parra <gonzzaparra@gmail.com>
2024-05-21 17:56:23 -0300

    feat(ui): update header to improve ux
    Add logic to check if the current section is the same as the navItem link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 import { ThemeToggle } from '@/components/theme-toggle';
@@ -5,6 +8,34 @@ import { ThemeToggle } from '@/components/theme-toggle';
 import { cn } from '@/lib/utils';
 
 export function Header() {
+  const [currentSection, setCurrentSection] = useState('');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      let currentId = '';
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop - sectionHeight / 2) {
+          currentId = section.id;
+        }
+      });
+
+      setCurrentSection(currentId);
+    };
+
+    handleScroll();
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   const navItems = [
     { title: 'About', label: 'about', url: '#about' },
     { title: 'Career', label: 'career', url: '#career' },
@@ -21,8 +52,10 @@ export function Header() {
             aria-label={item.label}
             href={item.url}
             className={cn(
-              'relative block mx-1 transition-colors ease-in-out py-2 md:px-4 px-2 rounded-md text-sm font-semibold hover:bg-neutral-100 dark:hover:bg-neutral-800'
-              // TODO: Add statement to check if the current section is the same as the link
+              'relative block mx-1 transition-colors ease-in-out py-2 md:px-4 px-2 rounded-md text-sm font-semibold hover:bg-secondary',
+              {
+                'text-primary': currentSection === item.label,
+              }
             )}
           >
             {item.title}
