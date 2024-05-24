a30f3b9224a6bf8a20112f3033d6af4480a46c31
Gonzalo Parra <gonzzaparra@gmail.com>
2024-05-24 17:18:14 -0300

    feat(ui): update header to improve responsiveness and UX while seeing from mobile devices

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,10 @@ import Link from 'next/link';
 import { ThemeToggle } from '@/components/theme-toggle';
 import { LangSwitcher } from '@/components/lang-switcher';
 
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import { MenuIcon } from 'lucide-react';
+
 import { cn } from '@/lib/utils';
 
 export function Header() {
@@ -48,7 +52,7 @@ export function Header() {
 
   return (
     <header className='fixed top-0 z-10 flex items-center justify-center w-full mx-auto bg-background border-b py-2'>
-      <nav className='flex justify-center items-center'>
+      <nav className='hidden sm:flex justify-center items-center'>
         {navItems.map((item) => (
           <Link
             key={item.label}
@@ -69,6 +73,40 @@ export function Header() {
           <LangSwitcher />
         </div>
       </nav>
+      <div className='flex sm:hidden justify-between w-full px-2'>
+        <div className='flex items-start'>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant='outline'>
+                <MenuIcon className='h-[1.2rem] w-[1.2rem]' />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side='left'>
+              <div className='grid gap-4 py-4'>
+                {navItems.map((item) => (
+                  <Link
+                    key={item.label}
+                    aria-label={item.label}
+                    href={item.url}
+                    className={cn(
+                      'relative block mx-1 transition-colors ease-in-out py-2 md:px-4 px-2 rounded-md text-sm font-semibold hover:bg-secondary',
+                      {
+                        'text-primary': currentSection === item.label,
+                      }
+                    )}
+                  >
+                    {item.title}
+                  </Link>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+        <div className='flex items-start space-x-2'>
+          <ThemeToggle />
+          <LangSwitcher />
+        </div>
+      </div>
     </header>
   )
 }
