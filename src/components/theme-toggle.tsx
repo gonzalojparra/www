05c47d1fcdadb0ab33f0b5c8fbe3b38b7a3414fa
Gonzalo Parra <gonzzaparra@gmail.com>
2024-08-05 19:18:25 -0300

    feat(theme): add view transition on theme-toggle component

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -8,6 +8,16 @@ import { Button } from '@/components/ui/button';
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
+  const toggleTheme = () => {
+    if (!document.startViewTransition) {
+      setTheme(theme === 'light' ? 'dark' : 'light');
+    } else {
+      document.startViewTransition(() => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+      });
+    }
+  };
+
   return (
     <div className='ml-2'>
       <Button
@@ -15,7 +25,7 @@ export function ThemeToggle() {
         className='rounded-full'
         size='icon'
         variant='ghost'
-        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        onClick={toggleTheme}
       >
         <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
         <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
