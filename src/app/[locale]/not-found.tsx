2ad212c25cbbbc2fc01befa0d3405c7b4c6551db
Gonzalo Parra <gonzzaparra@gmail.com>
2024-08-28 23:19:34 -0300

    chore: update not found component

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,31 +1,25 @@
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
-import PixelStars from '@/components/pixel-stars';
-
 export default function NotFoundPage() {
   const t = useTranslations();
 
   return (
-    <>
-      <div className='absolute left-0 top-0 h-full w-full object-cover'>
-        <PixelStars />
-      </div>
-
+    <div>
       <div className='z-10 flex flex-col items-center'>
-        <h1 className='flex items-center gap-x-4 text-balance text-5xl font-bold text-white'>
+        <h1 className='flex items-center gap-x-4 text-balance text-5xl font-bold'>
           {t('not-found.title')}
         </h1>
-        <p className='mx-auto max-w-3xl pt-4 text-center font-mono text-white/70 md:mb-4'>
+        <p className='mx-auto max-w-3xl pt-4 text-center font-mono opacity-70 md:mb-4'>
           {t('not-found.description')}
         </p>
 
         <div className='flex flex-wrap items-center pt-2 md:pt-0'>
-          <Link className='text-white hover:underline hover:underline-offset-4' href='/'>
+          <Link className='hover:underline hover:underline-offset-4' href='/'>
             {t('not-found.button')}
           </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
